015797cb1d3af8f57408a0685e18a28c9063376b
jehlp <jehlpxyz@gmail.com>
2024-12-01 01:24:23 -0500

    revert theme.js

--- assets/js/JWTheme.js
+++ assets/js/JWTheme.js
@@ -4,21 +4,15 @@ const updateTheme = (theme) => {
     if (themeIcon) {
         themeIcon.className = theme === 'light' ? 'fas fa-moon' : 'fas fa-sun';
     }
-};
-
+}
+ 
 const toggleTheme = () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    const newTheme = document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
     localStorage.setItem('theme', newTheme);
     updateTheme(newTheme);
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleButton = document.querySelector('.theme-toggle');
-    if (themeToggleButton) {
-        themeToggleButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            toggleTheme();
-        });
-    }
-});
+}
+ 
+updateTheme(localStorage.getItem('theme') || 'dark');
+document.addEventListener('DOMContentLoaded', () => 
+    document.querySelector('.theme-toggle')?.addEventListener('click', toggleTheme)
+);
